f2b9e6722130333517d1b6e9f5191046ed6e536a
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-03 21:39:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  descrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from "./features/counter/counterSlice";
 
 function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  const handleTambah = () => {
+    dispatch(increment());
+  };
+  const handleKurang = () => {
+    dispatch(descrement());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button aria-label="Increment value" onClick={handleTambah}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={handleKurang}>
+          Decrement
+        </button>
+        <button
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Add Amount
+        </button>
+      </div>
     </div>
   );
 }
